d76fd86c49f5dc7e5a08e3b475ba83487b370b28
Viktor Oblomov <viktorall88@gmail.com.com>
2023-06-03 07:06:18 +0300

    fix description

--- main.py
+++ main.py
@@ -264,18 +264,19 @@ def download_video(message, url, audio=False):
 @bot.message_handler(commands=["start", "help"])
 def send_start(message):
     if message.text == "/start":
-        text = """🤖 This bot can download videos and audios from YouTube.
+        text = """🇺🇸 This bot can download videos and music from YouTube.
 Send the link, choose the format and get your file.
 
-🤖 Этот бот может скачивать видео и аудио из Ютуба.
+🇷🇺 Этот бот может скачивать видео и музыку из Ютуба.
 Отправь ссылку, выбери формат и получи свой файл.
 
-/help - О боте
+/help - about bot | о боте
+justsave.app - app | приложение
 
-👇Отправь ссылку и получи свой файл👇
+👇 send me the link | отправь мне ссылку 👇
 """
     elif message.text == "/help":
-        text = """🔥 JustSaveIt_YouTube может скачать для вас видео ролики и аудио из YouTube.
+        text = """🔥 JustSave YouTube может скачать для вас видео ролики и аудио из YouTube.
 
 Как пользоваться:
   1. Зайдите в YouTube.
